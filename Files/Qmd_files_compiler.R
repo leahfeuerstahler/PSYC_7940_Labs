8d25300ca66192b1c054feabb42b16d9fe2feada
Leah Feuerstahler <lfeuerstahler@fordham.edu>
2026-01-04 16:24:15 -0500

    restructure course

--- Files/Qmd_files_compiler.R
+++
@@ -1,8 +0,0 @@
-# run to compile all .qmd files in directory (used when theme updates)
-# assumes folder of file is working directory
-
-qmd_files <- list.files()[grep(".qmd", list.files())]
-
-for(i in 1:length(qmd_files)){
-  quarto::quarto_render(input = qmd_files[i])
-}
